b996cafa71a5a94a38299646d84879d52683128b
Alex Davis <alex32davis@yahoo.com>
2024-04-23 14:54:09 -0600

    feat: :sparkles: added ExpirableEntry object to hold expiration time, key, and value. Added cache code

--- src/main/java/io/collective/SimpleAgedCache.java
+++ src/main/java/io/collective/SimpleAgedCache.java
@@ -1,31 +1,66 @@
 package io.collective;
 
 import java.time.Clock;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SimpleAgedCache {
-    private  Clock myClock;
+    private Clock myClock;
+    private List<ExpirableEntry> myList;
 
     public SimpleAgedCache(Clock clock) {
         this.myClock = clock;
+        this.myList = new ArrayList<ExpirableEntry>();
     }
 
-
     public SimpleAgedCache() {
-        
+        this.myClock = Clock.systemUTC();
+        this.myList = new ArrayList<ExpirableEntry>();
     }
 
     public void put(Object key, Object value, int retentionInMillis) {
+        removeExpired();
+        ExpirableEntry item = new ExpirableEntry(key, value, retentionInMillis, myClock);
+        myList.add(item);
     }
 
     public boolean isEmpty() {
-        return false;
+        removeExpired();
+        return myList.size() == 0;
     }
 
     public int size() {
-        return 0;
+        removeExpired();
+        return myList.size();
     }
 
     public Object get(Object key) {
+        removeExpired();
+        for (ExpirableEntry item : myList) {
+            if (item.key == key)
+                return item.value;
+        }
         return null;
+    }
+
+    public void removeExpired() {
+        myList.removeIf(b -> b.isExpired(myClock));
+    }
+}
+
+class ExpirableEntry {
+
+    Object key;
+    Object value;
+    long expireMillisecond;
+
+    public ExpirableEntry(Object key, Object value, int retentionInMillis, Clock clock) {
+        this.key = key;
+        this.value = value;
+        this.expireMillisecond = clock.millis() + retentionInMillis;
+    }
+
+    public boolean isExpired(Clock clock) {
+        return clock.millis() > expireMillisecond;
     }
 }
